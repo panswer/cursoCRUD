e16f401b4b7ee21fc9d05fe7762dd6bf48832a86
Ricardo <panswer.rjmm@gmail.com>
2020-06-06 18:26:32 -0400

    Base de datos mongodb configuracion base

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 const app = require('./server');
+
+require('./database');
 
 app.listen(app.get('port'), err => {
     console.log(`Server on port ${app.get('port')}`);
