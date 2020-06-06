6b732a46288584563cf479a578af0747b5a7e4fa
Ricardo <panswer.rjmm@gmail.com>
2020-06-06 19:02:58 -0400

    configuracion basica de handlebars

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 // Initializations
 const app = express();
 
 // Settings
-app.get('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main.hbs',
+    layoutsDir: path.resolve(app.get('views') + '/layouts'),
+    partialsDir: path.resolve(app.get('views') + '/partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({ extended: false })); //nos ayuda a recibir los datos en json
@@ -16,7 +24,7 @@ app.use(express.urlencoded({ extended: false })); //nos ayuda a recibir los dato
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.render('index');
 });
 
 // Static files
